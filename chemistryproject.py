f950fd3b88f9b536a4373a867568711e6fbd3e46
CipherNinja <91175201+CipherNinja@users.noreply.github.com>
2022-02-18 18:49:49 +0530

    Add files via upload

---
+++ chemistryproject.py
@@ -0,0 +1,420 @@
+import math as m 
+
+
+from tabulate import tabulate as tab
+head = ["CYPHERNINJA --- FOLLOW ME ON INSTAGRAM"]
+
+data = [{"My INSTAGRAM ID is @cypherninja1010"}]
+
+print(tab(data,headers=head,tablefmt="grid"))
+
+ 
+def speed():
+	formulae = print(m.pow(10,8)*3,"is the value of speed")
+	print(formulae)
+i = input("For random value of light chose '1' otheerwise chose '2' :::>  ")
+i = int(i)
+
+
+if i == 1:
+	speed()
+if i == 2:
+	j = int(input('give the value of light withouth any + - * ^ % $ # @ ! symbol :: '))
+	print(m.pow(10,8)*j)
+elif i > 2:
+	print("you have 2 options only chose one of them (1 for random value / 2 for your value)")
+else:
+	speed()
+
+
+head = ["CYPHERNINJA --- FOLLOW ME ON INSTAGRAM"]
+
+data = [{"PLEASE WAIT !"},{"LOADING ----100%"},{"YOUR PROGRAMME IS READY TO USE NOW ."}]
+
+print(tab(data,headers=head,tablefmt="grid"))
+
+
+
+
+#--------------------------------------MASS SECTION (IDENTIFICATION)-------------------------------------------------------------------------------
+
+def MASS(m):
+	mass = m*2
+	print("atomis mass of atom is",mass)
+
+i = int(input("give the mass numbber of ATOM and take IDENTIFICATION from periodic table ::: >> "))
+if i == 1:
+	print("hydrogen with atomic number",i)
+	MASS(i)
+elif i == 2:
+	print("Helium with atomic number",i)
+	MASS(i)
+ 
+elif i == 3:
+	print("lithium with atomic number",i)
+	MASS(i)
+elif i == 4:
+	print("Berylium with atomic number",i)
+	MASS(i)
+elif i == 5:
+	print("Boron with atomic number",i)
+	MASS(i)
+elif i == 6:
+	print("Carbon with atomic number",i)
+	MASS(i)
+elif i == 7:
+	print("Nitrogen with atomic number",i)
+	MASS(i)
+elif i == 8:
+	print("Oxygen with atomic number",i)
+	MASS(i)
+elif i == 9:
+	print("Flurine with atomic number",i)
+	MASS(i)
+elif i == 10:
+	print("Neon with atomic number",i)
+	MASS(i)
+elif i == 11:
+	print("Sodium with atomic number",i)
+	MASS(i)
+elif i == 12:
+	print("Magnesium with atomic number",i)
+	MASS(i)
+elif i == 13:
+	print("Aluminium with atomic number",i)
+	MASS(i)
+elif i == 14:
+	print("Silicon with atomic number",i)
+	MASS(i)
+elif i == 15:
+	print("Phosphorus with atomic number",i)
+	MASS(i)
+elif i == 16:
+	print("Sulfur with atomic number",i)
+	MASS(i)
+elif i == 17:
+	print("Clorine with atomic number",i)
+	MASS(i)
+elif i == 18:
+	print("Argon with atomic number",i)
+	MASS(i)
+elif i == 19:
+	print("Pottasium with atomic number",i)
+	MASS(i)
+elif i == 20:
+	print("Calcium with atomic number",i)
+	MASS(i)
+elif i == 21:
+	print("Scandium with atomic number",i)
+	MASS(i)
+elif i == 22:
+	print("Titanium with atomic number",i)
+	MASS(i)
+elif i == 23:
+	print("Titanium with atomic number",i)
+	MASS(i)
+elif i == 24:
+	print("Chromium with atomic number",i)
+	MASS(i)
+elif i == 25:
+	print("Magnesium with atomic number",i)
+	MASS(i)
+elif i == 26:
+	print("Iron with atomic number",i)
+	MASS(i)
+elif i == 27:
+	print("Cobalt with atomic number",i)
+	MASS(i)
+elif i == 28:
+	print("Nickel with atomic number",i)
+	MASS(i)
+elif i == 29:
+	print("Copper with atomic number",i)
+	MASS(i)
+elif i == 30:
+	print("Zinc with atomic number",i)
+	MASS(i)
+elif i == 31:
+	print("Galium with atomic number",i)
+	MASS(i)
+elif i == 32:
+	print("Germanium with atomic number",i)
+	MASS(i)
+elif i == 33:
+	print("Arsenic with atomic number",i)
+	MASS(i)
+elif i == 34:
+	print("Selinium with atomic number",i)
+	MASS(i)
+elif i == 35:
+	print("Bromine with atomic number",i)
+	MASS(i)
+elif i == 36:
+	print("Krypton with atomic number",i)
+	MASS(i)
+elif i == 37:
+	print("Rubidium with atomic number",i)
+	MASS(i)
+elif i == 38:
+	print("Strontium with atomic number",i)
+	MASS(i)
+elif i == 39:
+	print("Yattrium with atomic number",i)
+	MASS(i)
+elif i == 40:
+	print("Zirconium with atomic number",i)
+	MASS(i)
+elif i == 41:
+	print("Niobium with atomic number",i)
+	MASS(i)
+elif i == 42:
+	print("Molybednum with atomic number",i)
+	MASS(i)
+elif i == 43:
+	print("Technetium with atomic number",i)
+	MASS(i)
+elif i == 44:
+	print("Ruthenium with atomic number",i)
+	MASS(i)
+elif i == 45:
+	print("Rhodium with atomic number",i)
+	MASS(i)
+elif i == 46:
+	print("Palladium with atomic number",i)
+	MASS(i)
+elif i == 47:
+	print("Silver with atomic number",i)
+	MASS(i)
+elif i == 48:
+	print("Cadmium with atomic number",i)
+	MASS(i)
+elif i == 49:
+	print("Tin Indium with atomic number",i)
+	MASS(i)
+elif i == 50:
+	print("Tin with atomic number",i)
+	MASS(i)
+elif i == 51:
+	print("Antimony with atomic number",i)
+	MASS(i)
+elif i == 52:
+	print("Tellurium with atomic number",i)
+	MASS(i)
+elif i == 53:
+	print("Iodine with atomic number",i)
+	MASS(i)
+
+elif i == 54:
+	print("Xenon with atomic number",i)
+	MASS(i)
+elif i == 55:
+	print("Caesium with atomic number",i)
+	MASS(i)
+elif i == 56:
+	print("Barium with atomic number",i)
+	MASS(i)
+elif i == 57:
+	print("Lanthanum with atomic number",i)
+	MASS(i)
+elif i == 58:
+	print("Cerium with atomic number",i)
+	MASS(i)
+elif i == 59:
+	print("Praseodymium with atomic number",i)
+	MASS(i)
+elif i == 60:
+	print("Neodymium with atomic number",i)
+	MASS(i)
+
+elif i == 61:
+	print("Prothmium with atomic number",i)
+	MASS(i)
+    
+elif i == 62:
+	print("Samarium with atomic number",i)
+	MASS(i)
+elif i == 63:
+	print("Europium with atomic number",i)
+	MASS(i)
+elif i == 64:
+	print("Gadolinium with atomic number",i)
+	MASS(i)
+elif i == 65:
+	print("Terbium with atomic number",i)
+	MASS(i)
+elif i == 66:
+	print("Dysprosium with atomic number",i)
+	MASS(i)
+elif i == 67:
+	print("Holmium with atomic number",i)
+	MASS(i)
+elif i == 68:
+	print("Erbium with atomic number",i)
+	MASS(i)
+elif i == 69:
+	print("Ytterbium with atomic number",i)
+	MASS(i)
+elif i == 70:
+	print("LUtetium with atomic number",i)
+	MASS(i)
+elif i == 71:
+	print("Hadnium Lutetium with atomic number",i)
+	MASS(i)
+elif i == 72:
+	print("Hadnium with atomic number",i)
+	MASS(i)
+elif i == 73:
+	print("Tungsten Tantalum with atomic number",i)
+	MASS(i)
+elif i == 74:
+	print("Rhenium Tungsten with atomic number",i)
+	MASS(i)
+elif i == 75:
+	print("Rhenium with atomic number",i)
+	MASS(i)
+elif i == 76:
+	print("Osmium with atomic number",i)
+	MASS(i)
+elif i == 77:
+	print("Iridium with atomic number",i)
+	MASS(i)
+elif i == 78:
+	print("Platinum with atomic number",i)
+	MASS(i)
+elif i == 79:
+	print("Gold with atomic number",i)
+	MASS(i)
+elif i == 80:
+	print("Mercury with atomic number",i)
+	MASS(i)
+elif i == 81:
+	print("Thallium with atomic number",i)
+	MASS(i)
+elif i == 82:
+	print("Lead with atomic number",i)
+	MASS(i)
+elif i == 83:
+	print("Bismuth with atomic number",i)
+	MASS(i)
+elif i == 84:
+	print("Polonium with atomic number",i)
+	MASS(i)
+elif i == 85:
+	print("AStatin with atomic number",i)
+	MASS(i)
+elif i == 86:
+	print("Randon with atomic number",i)
+	MASS(i)
+elif i == 87:
+	print("Francium with atomic number",i)
+	MASS(i)
+elif i == 88:
+	print("Radium with atomic number",i)
+	MASS(i)
+elif i == 89:
+	print("Actinium with atomic number",i)
+	MASS(i)
+elif i == 104:
+	print("Rutherford.., with atomic number",i)
+	MASS(i)
+elif i == 105:
+	print("Dubnium with atomic number",i)
+	MASS(i)
+elif i == 106:
+	print("Seaborgium with atomic number",i)
+	MASS(i)
+elif i == 107:
+	print("Bohrium with atomic number",i)
+	MASS(i)
+elif i == 108:
+	print("Hassium with atomic number",i)
+	MASS(i)
+elif i == 109:
+	print("Meritnerium with atomic number",i)
+	MASS(i)
+elif i == 110:
+	print("Darmstadtium with atomic number",i)
+	MASS(i)
+elif i == 111:
+	print("Roentgenium with atomic number",i)
+	MASS(i)
+elif i == 112:
+	print("Copernicium with atomic number",i)
+	MASS(i)
+elif i == 113:
+	print("Nihonium with atomic number",i)
+	MASS(i)
+elif i == 114:
+	print("Flerobium with atomic number",i)
+	MASS(i)
+elif i == 115:
+	print("Moscovium with atomic number",i)
+	MASS(i)
+elif i == 116:
+	print("Livermorium with atomic number",i)
+	MASS(i)
+elif i == 117:
+	print("Tennessine with atomic number",i)
+	MASS(i)
+elif i == 118:
+	print("Oganesson with atomic number",i)
+	MASS(i)
+elif i == 90:
+	print("Thorium with atomic number",i)
+	MASS(i)
+elif i == 91:
+	print("Protactinium with atomic number",i)
+	MASS(i)
+elif i == 92:
+	print("Uranium with atomic number",i)
+	MASS(i)
+elif i == 93:
+	print("Neptunium with atomic number",i)
+	MASS(i)
+elif i == 94:
+	print("Plutonium with atomic number",i)
+	MASS(i)
+elif i == 95:
+	print("Americium with atomic number",i)
+	MASS(i)
+elif i == 96:
+	print("Curium with atomic number",i)
+	MASS(i)
+elif i == 97:
+	print("	Berkelium with atomic number",i)
+	MASS(i)
+elif i == 98:
+	print("Californium with atomic number",i)
+	MASS(i)
+elif i == 99:
+	print("Einsteinium with atomic number",i)
+	MASS(i)
+elif i == 100:
+	print("Fermium with atomic number",i)
+	MASS(i)
+elif i == 101:
+	print("Mendelevium with atomic number",i)
+	MASS(i)
+elif i == 102:
+	print("Nobelium with atomic number",i)
+	MASS(i)
+elif i == 103:
+	print("Lawrencium with atomic number",i)
+	MASS(i)
+elif i > 118:
+	print("The element with mass number ",i," is not discovered yet !")
+else:
+	print("thanks for breaking this programme you will be awarded for this ")
+	MASS(i)
+
+
+head = ["CYPHERNINJA --- FOLLOW ME ON INSTAGRAM"]
+
+data = [{"My INSTAGRAM ID is @cypherninja1010"}]
+
+print(tab(data,headers=head,tablefmt="grid"))
+
+
+
+#-------------------------------->>>>>>>>|Created By: Priyesh-Pandey|<<<<<<<<---------------------------------#
+
